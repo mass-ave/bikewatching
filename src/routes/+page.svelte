451504ed8c15cf5b2356d3c1412a1e57f1f54776
acevedo <acevedo@mit.edu>
2025-04-06 10:40:09 -0400

    changed main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Bikewatching</h1>
+<p>This is the brief description of what it does</p>
